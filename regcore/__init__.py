0b4d021503257fbeb326c189d177ea625baa45fc
CM Lubinski <cm.lubinski@gmail.com>
2013-08-22 23:41:11 +0200

    Provide some sample values to be used by the buildout django

--- regcore/__init__.py
+++ regcore/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+#from flask import Flask
 
-app = Flask('eregs-api')
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+#app = Flask('eregs-api')
+#app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
---
+++ regcore/example_settings.py
@@ -0,0 +1,9 @@
+INSTALLED_APPS = [
+    'regcore'
+]
+
+SECRET_KEY = 'v^p)1cwc)%td*szt7lt-(nl=bf)k07t%65*t(mi1f!*18dz9m@'
+
+DATABASES = {}
+
+TEST_RUNNER = 'testing.DatabaselessTestRunner'
