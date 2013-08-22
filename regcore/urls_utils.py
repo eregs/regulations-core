f398890a9c25e48c947abfeb6b0ecb7acbf15406
CM Lubinski <cm.lubinski@gmail.com>
2013-08-23 00:18:29 +0200

    Handlers are starting to migrate into views

---
+++ regcore/urls_utils.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.http import Http404
+
+def by_verb_url(regex, name, **by_verb):
+    def wrapper(request, *args, **kwargs):
+        verb = request.method.upper()
+        if verb in by_verb:
+            by_verb[verb](request, *args, **kwargs)
+        else:
+            raise Http404
+    url(regex, wrapper, name=name)
