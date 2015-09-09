a7ae972f663af652f09425c082daf2bbd585258d
CM Lubinski <christopher.lubinski@gsa.gov>
2015-09-09 20:47:09 +0000

    Use setup.py to define requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "regcore",
-    version = "0.1.0", 
-    license = "public domain", 
-    packages = find_packages()
+    name="regcore",
+    version="2.0.0",
+    license="public domain",
+    packages=find_packages(),
+    install_requires=[
+        'anyjson',
+        'django==1.8',
+        'jsonschema'
+    ]
 )
