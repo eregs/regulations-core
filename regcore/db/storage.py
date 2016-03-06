c6b93dd639a064171705ca131074b5bbc8974b20
CM Lubinski <christopher.lubinski@gsa.gov>
2016-03-06 18:07:58 +0000

    Set defaults for storage backends
    We can't easily add new data types for our storage backends as the
    configuration is flat out *replaced* when modified. This patch maintains that
    behavior while providing a fallback storage system at instantiation time.

--- regcore/db/storage.py
+++ regcore/db/storage.py
@@ -5,8 +5,12 @@ from django.conf import settings
 
 def select_for(data_type):
     """The storage class for each datatype is defined in a settings file. This
-    will look up the appropriate storage backend and instantiate it"""
-    module, class_name = settings.BACKENDS[data_type].rsplit('.', 1)
+    will look up the appropriate storage backend and instantiate it. If none
+    is found, this will default to the Django ORM versions"""
+    class_str = settings.BACKENDS.get(
+        data_type,
+        'regcore.db.django_models.DM' + data_type.capitalize())
+    module, class_name = class_str.rsplit('.', 1)
     module = import_module(module)
     return getattr(module, class_name)()
 
@@ -14,3 +18,4 @@ for_regulations = select_for('regulations')
 for_layers = select_for('layers')
 for_notices = select_for('notices')
 for_diffs = select_for('diffs')
+for_preambles = select_for('preambles')
