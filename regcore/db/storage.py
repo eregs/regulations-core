f181d3118f4a4f33aea58b8b80093a4d17bbb1eb
CM Lubinski <christopher.lubinski@gsa.gov>
2016-03-08 20:45:05 +0000

    Use Django's import_string
    Thanks for the suggestion @jmcarp

--- regcore/db/storage.py
+++ regcore/db/storage.py
@@ -1,14 +1,11 @@
-from importlib import import_module
-
 from django.conf import settings
+from django.utils.module_loading import import_string
 
 
 def select_for(data_type):
     """The storage class for each datatype is defined in a settings file. This
     will look up the appropriate storage backend and instantiate it"""
-    module, class_name = settings.BACKENDS[data_type].rsplit('.', 1)
-    module = import_module(module)
-    return getattr(module, class_name)()
+    return import_string(settings.BACKENDS[data_type])()
 
 for_regulations = select_for('regulations')
 for_layers = select_for('layers')
