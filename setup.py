aa055bb91bc91f6c4c2e0172374d681e09017cf3
CM Lubinski <cmc333333@gmail.com>
2017-07-12 11:49:11 -0400

    Bump to version 3.1.0
    # New features
    * Django 1.10 and 1.11 support
    
    # Misc
    * Switch to tox for testing across multiple Django versions
    * Drop requirements files to discourage running as an app

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="regcore",
-    version="3.0.0",
+    version="3.1.0",
     license="public domain",
     packages=find_packages(),
     include_package_data=True,
