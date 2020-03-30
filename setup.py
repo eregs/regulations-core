8c1810114c8aa211da41b591490ece7343499eb3
Priyanka Kasireddy <pkasireddy@fec.gov>
2020-03-30 19:08:40 -0400

    update pkg django-mptt v0.11.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=[
         'cached_property',
         'django~=2.0.0',
-        'django-mptt~=0.8.6',
+        'django-mptt~=0.11.0',
         'jsonschema',
         'six',
         'webargs',
